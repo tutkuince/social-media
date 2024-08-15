616d4e416e0e3788f4d51248320a8d0461aa9dbb
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-15 13:51:17 +0300

    Auh Entity

---
+++ auth-service/src/main/java/com/incetutku/entity/Auth.java
@@ -0,0 +1,25 @@
+package com.incetutku.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tbl_auth")
+public class Auth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private Boolean isActive;
+    private Long createdAt;
+
+}

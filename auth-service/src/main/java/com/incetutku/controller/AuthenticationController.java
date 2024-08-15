bcfb8ef5bec4c45ece5a50a6b40ac10045fb4237
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-16 00:17:14 +0300

    login and url set

--- auth-service/src/main/java/com/incetutku/controller/AuthenticationController.java
+++ auth-service/src/main/java/com/incetutku/controller/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.incetutku.controller;
 
+import com.incetutku.config.RestAPIs;
+import com.incetutku.dto.request.LoginRequestDTO;
 import com.incetutku.dto.request.RegisterRequestDTO;
 import com.incetutku.entity.Authentication;
 import com.incetutku.service.AuthenticationService;
@@ -11,16 +13,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.incetutku.config.RestAPIs.*;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping()
+@RequestMapping(AUTH_SERVICE)
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
-    @PostMapping
+    @PostMapping(AUTH_SERVICE_REGISTER)
     public ResponseEntity<Authentication> register(@RequestBody RegisterRequestDTO registerRequestDTO) {
         if (!registerRequestDTO.password().equals(registerRequestDTO.rePassword()))
             throw new RuntimeException("Passwords do not match");
         return new ResponseEntity<>(authenticationService.register(registerRequestDTO), HttpStatus.CREATED);
+    }
+
+    @PostMapping(AUTH_SERVICE_LOGIN)
+    public ResponseEntity<Boolean> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        return ResponseEntity.ok(authenticationService.login(loginRequestDTO));
     }
 }

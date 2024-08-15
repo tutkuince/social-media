6e43e1eb0c6ef00e216038483144132d3e535d62
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-15 23:59:50 +0300

    register

--- auth-service/src/main/java/com/incetutku/controller/AuthenticationController.java
+++ auth-service/src/main/java/com/incetutku/controller/AuthenticationController.java
@@ -1,10 +1,13 @@
 package com.incetutku.controller;
 
+import com.incetutku.dto.request.RegisterRequestDTO;
 import com.incetutku.entity.Authentication;
 import com.incetutku.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
-    /*@PostMapping
-    public ResponseEntity<Authentication> register() {
-
-    }*/
+    @PostMapping
+    public ResponseEntity<Authentication> register(@RequestBody RegisterRequestDTO registerRequestDTO) {
+        if (!registerRequestDTO.password().equals(registerRequestDTO.rePassword()))
+            throw new RuntimeException("Passwords do not match");
+        return new ResponseEntity<>(authenticationService.register(registerRequestDTO), HttpStatus.CREATED);
+    }
 }

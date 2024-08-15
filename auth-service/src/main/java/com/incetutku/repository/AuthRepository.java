0a97a3750e08309bfe1f5c44aa8ff7f19c1aaecc
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-15 13:56:04 +0300

    update for repo

--- auth-service/src/main/java/com/incetutku/repository/AuthRepository.java
+++
@@ -1,7 +0,0 @@
-package com.incetutku.repository;
-
-import com.incetutku.entity.Auth;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthRepository extends JpaRepository<Auth, Long> {
-}
---
+++ auth-service/src/main/java/com/incetutku/repository/AuthenticationRepository.java
@@ -0,0 +1,7 @@
+package com.incetutku.repository;
+
+import com.incetutku.entity.Authentication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthenticationRepository extends JpaRepository<Authentication, Long> {
+}

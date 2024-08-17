c7890e09db6138d34bc1572abab8a6fbb0773f6b
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-17 21:25:48 +0300

    authentication service

--- auth-service/src/main/java/com/incetutku/service/AuthenticationService.java
+++ auth-service/src/main/java/com/incetutku/service/AuthenticationService.java
@@ -1,8 +1,10 @@
 package com.incetutku.service;
 
+import com.incetutku.dto.request.CreateUserRequestDTO;
 import com.incetutku.dto.request.LoginRequestDTO;
 import com.incetutku.dto.request.RegisterRequestDTO;
 import com.incetutku.entity.Authentication;
+import com.incetutku.manager.UserProfileManager;
 import com.incetutku.repository.AuthenticationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final AuthenticationRepository authenticationRepository;
+    private final UserProfileManager userProfileManager;
 
     public Authentication register(RegisterRequestDTO registerRequestDTO) {
-        return authenticationRepository.save(Authentication.builder()
+        Authentication authentication = authenticationRepository.save(Authentication.builder()
                 .username(registerRequestDTO.username())
                 .email(registerRequestDTO.email())
                 .password(registerRequestDTO.password())
                 .build());
+        userProfileManager.createUser(CreateUserRequestDTO.builder()
+                .authenticationId(authentication.getId())
+                .email(authentication.getEmail())
+                .username(authentication.getUsername())
+                .build());
+        return authentication;
     }
 
     public Boolean login(LoginRequestDTO loginRequestDTO) {
